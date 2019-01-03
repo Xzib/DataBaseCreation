df0fd55dcc0cae4021f8b17f7f1cfcf4cc8daa05
Xzib <syedzohaibali94@gmail.com>
2019-01-03 16:45:04 +0500

    Add project files.

---
+++ DataBaseCreation/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data.OleDb;
+
+namespace DataBaseCreation
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string connectionString = @"Provider = Microsoft.ACE.OLEDB.12.0; Data Source = C:\Users\Zohaib\Desktop\TestDataBase.accdb;Persist Security Info = False;";
+
+            OleDbConnection connection = new OleDbConnection(connectionString);
+
+            try
+            {
+                connection.Open();
+                MessageBox.Show("Connection established");
+                connection.Close();
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show( ex.ToString());
+            }
+            
+        }
+    }
+}
